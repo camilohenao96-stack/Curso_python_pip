28ec37cc26b2b1a181cf4ace480b081c6cb5d3c0
echenao <camilohenao96@gmail.com>
2026-03-17 15:18:22 -0500

    Uso de requests

--- web-server/store.py
+++ web-server/store.py
@@ -1,6 +1,16 @@
+import json
 import requests
 
 def get_categories():
-    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    r = requests.get('https://api.escuelajs.co/api/v1/categories', verify=False)
     print (r.status_code)
     print(r.text)
+    print(type(r.text))
+    print("\n")
+    categories = r.json()
+    
+    with open("prueba.json", "w", encoding="utf-8") as file:
+        json.dump(categories, file, indent=4, ensure_ascii=False)
+    
+    for category in categories:
+        print(category['name'])
